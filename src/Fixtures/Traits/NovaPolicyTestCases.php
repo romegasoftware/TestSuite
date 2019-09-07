b97787eced69cb84f3ae826f3e203e5ddbf4596b
Naoray Kroaths <krishan.koenig@gmail.com>
2019-09-07 23:16:20 +0000

    Dev

--- src/Fixtures/Traits/NovaPolicyTestCases.php
+++ src/Fixtures/Traits/NovaPolicyTestCases.php
@@ -10,7 +10,7 @@ trait NovaPolicyTestCases
 
     public $permissionsClass = \App\Enums\Permissions::class;
 
-    public function setup_resource()
+    public function setupResource()
     {
         $user = factory(\App\User::class)->create();
         $user->tenants()->save($this->tenantAdmin);
@@ -22,74 +22,68 @@ trait NovaPolicyTestCases
     /** @test **/
     public function it_can_view_any()
     {
-        [$user, $resource] = $this->setup_resource();
+        [$user, $resource] = $this->setupResource();
 
         $this->expectStatusCode(403)
-            ->getResource('', $user)
-            ->assertStatus(403);
+            ->getResource('', $user);
 
         $user->givePermissionTo($this->permissionsClass::read($this->modelClass));
 
-        $this->getResource('', $user)->assertOk();
+        $this->getResource('', $user);
     }
 
     /** @test **/
     public function it_can_view_model()
     {
-        [$user, $resource] = $this->setup_resource();
+        [$user, $resource] = $this->setupResource();
 
         $this->expectStatusCode(403)
-            ->getResource($resource->id, $user)
-            ->assertStatus(403);
+            ->getResource($resource->id, $user);
 
         $user->givePermissionTo($this->permissionsClass::read($this->modelClass));
 
-        $this->getResource($resource->id, $user)->assertOk();
+        $this->getResource($resource->id, $user);
     }
 
     /** @test */
     public function it_cant_create_a_resource_without_permission()
     {
-        [$user, $resource] = $this->setup_resource();
+        [$user, $resource] = $this->setupResource();
 
         $this->expectStatusCode(403)
-            ->storeResource([], $user)
-            ->assertStatus(403);
+            ->storeResource([], $user);
 
         $user->givePermissionTo($this->permissionsClass::read($this->modelClass));
         $user->givePermissionTo($this->permissionsClass::create($this->modelClass));
 
-        $this->storeResource([], $user)
-            ->assertStatus(201);
+        $this->storeResource([], $user);
     }
 
     /** @test */
     public function it_cant_update_a_resource_without_permission()
     {
-        [$user, $resource] = $this->setup_resource();
+        [$user, $resource] = $this->setupResource();
 
         $this->expectStatusCode(403)
             ->updateResource([
                 'id' => $resource->id,
-            ], $user)
-            ->assertStatus(403);
+            ], $user);
 
         $user->givePermissionTo($this->permissionsClass::read($this->modelClass));
         $user->givePermissionTo($this->permissionsClass::update($this->modelClass));
 
         $this->updateResource([
                 'id' => $resource->id,
-            ], $user)
-            ->assertStatus(200);
+            ], $user);
     }
 
     /** @test */
     public function it_cant_destroy_a_resource_without_permission()
     {
-        [$user, $resource] = $this->setup_resource();
+        [$user, $resource] = $this->setupResource();
 
         $this->deleteResource([$resource->id], $user);
-        if(in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->modelClass))){
+        if (in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->modelClass))) {
             $this->assertEquals($resource->fresh()->{$resource->getDeletedAtColumn()}, null);
         } else {
             $this->assertDatabaseHas($resource->getTable(), $resource->only('id'));
@@ -97,9 +91,9 @@ trait NovaPolicyTestCases
 
         $user->givePermissionTo($this->permissionsClass::read($this->modelClass));
         $user->givePermissionTo($this->permissionsClass::delete($this->modelClass));
-        
+
         $this->deleteResource([$resource->id], $user);
-        if(in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->modelClass))){
+        if (in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->modelClass))) {
             $this->assertSoftDeleted($resource->getTable(), $resource->only('id'));
         } else {
             $this->assertDatabaseMissing($resource->getTable(), $resource->only('id'));
