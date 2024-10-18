e8249c86c03f16c6f4ee14048bd7a4f2da250d3d
Braden Keith <bradenkeith@gmail.com>
2024-10-18 12:41:03 -0400

    Update TenantDomain.php

--- src/TenantDomain.php
+++ src/TenantDomain.php
@@ -8,18 +8,18 @@ use Illuminate\Support\Facades\URL;
 
 trait TenantDomain
 {
-	use ActingAsUser;
+    use ActingAsUser;
 
-	protected $tenant = [];
+    protected $tenant = [];
 
-	public function tenantDomain()
-	{
-        	$this->tenant = config('multitenancy.tenant_model')::factory()->create();
+    public function tenantDomain()
+    {
+        $this->tenant = config('multitenancy.tenant_model')::factory()->create();
 
-		$this->tenant->users()->save($this->actingAsUser());
+        $this->tenant->users()->save($this->actingAsUser());
 
-		$url = Request::getScheme().'://'.$this->tenant->domain.'.'.Request::getHost();
-		Config::set('app.url', $url);
-        	URL::forceRootUrl($url);
-	}
+        $url = Request::getScheme().'://'.$this->tenant->domain.'.'.Request::getHost();
+        Config::set('app.url', $url);
+        URL::forceRootUrl($url);
+    }
 }
