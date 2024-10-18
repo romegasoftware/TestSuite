dffa6a4d2a97d64eca38b7fa900e93e6f1aabedc
Braden Keith <bradenkeith@gmail.com>
2024-10-18 12:41:44 -0400

    Update SetupRomegaTestSuite.php

--- src/SetupRomegaTestSuite.php
+++ src/SetupRomegaTestSuite.php
@@ -4,16 +4,15 @@ namespace RomegaDigital\TestSuite;
 
 trait SetupRomegaTestSuite
 {
-	public function setupRomegaTestSuite($uses)
-	{
+    public function setupRomegaTestSuite($uses)
+    {
         //CustomerDomain uses ActingAsUser so we only need one
-		if (isset($uses[\RomegaDigital\TestSuite\TenantDomain::class])) {
-		    $this->tenantDomain();
-		} elseif (isset($uses[\RomegaDigital\TestSuite\TenantAdminDomain::class])) {
-		    $this->tenantAdminDomain();
-		}  elseif (isset($uses[\RomegaDigital\TestSuite\ActingAsUser::class])) {
-		    $this->actingAsUser();
-		} 
-	}
-
+        if (isset($uses[\RomegaDigital\TestSuite\TenantDomain::class])) {
+            $this->tenantDomain();
+        } elseif (isset($uses[\RomegaDigital\TestSuite\TenantAdminDomain::class])) {
+            $this->tenantAdminDomain();
+        } elseif (isset($uses[\RomegaDigital\TestSuite\ActingAsUser::class])) {
+            $this->actingAsUser();
+        }
+    }
 }
